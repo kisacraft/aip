02d9e0eec17e92b2bfac364ddd9bdc663e3e6993
Lazard <laseanus@gmail.com>
2023-09-25 11:58:33 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from random import randrange
+
+from test import power
+
+# f = open("temp.txt", "w")
+# f.write("hello")
+# f.close()
+
+f = open('temp.txt', "r")
+print(f.read())
+f.close()
+
+x: object = power(2, 2)
+print(x)
+
+y = randrange(1, 100)
+print(y)
